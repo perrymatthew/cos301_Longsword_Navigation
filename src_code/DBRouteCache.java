c268dc04c5ad5e62dc2885b9ea5657b880231285
Duart <duartbreedt@gmail.com>
2017-03-20 00:04:29 +0200

    manageRoutes function added
    Manages the route database.
    Deletes certain routes when there are too many routes.
    Parses JSON file and writes back to JSON file

--- src_code/DBRouteCache.java
+++ src_code/DBRouteCache.java
@@ -1,4 +1,26 @@
-public class DBRouteCache
-{
-    
+import java.io.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.*;
+import org.json.*;
+
+public class DBRouteCache {
+
+    private void manageRoutes () {
+    	try {
+	    	JSONParser parser = new JSONParser();
+	    	org.json.simple.JSONArray simpleArr = (org.json.simple.JSONArray) parser.parse(new FileReader("DBRouteCache.json"));
+	    	org.json.JSONArray arr =  new org.json.JSONArray(simpleArr.toJSONString());
+	    	arr = sortArrayByPopularity(arr);
+	    	while(arr.length() > 20) {
+		    	arr.remove(11);
+	    	} 
+	    	try (FileWriter file = new FileWriter("DBRouteCache.json")) {
+				file.write(arr.toString());
+			} catch (Exception e) {
+    			e.printStackTrace();	
+			}
+    	} catch (Exception e) {
+	    	e.printStackTrace();
+    	}
+    }
 }
