d211c80c2df88ffac39c612cb5a5d7586641be5f
Nathan Ngobale <youngjon01@gmail.com>
2017-03-23 00:47:00 +0200

    Update DBPreferences.java

--- src_code/DBPreferences.java
+++ src_code/DBPreferences.java
@@ -1,4 +1,19 @@
 public class DBPreferences implements Navigation
 {
+    JSONParser parser = new JSONParser();
     
+    public DBPreferences()
+    {
+       /* try{
+            Object obj = parser.parse(new FileReader());           
+            JSONObject jsonObject = (JSONObject) obj;
+            String start_point = (String) jsonObject.get("start_point");
+            String end_point = (String) jsonObject.get("end_point");
+            Integer popularity = (Integer) jsonObject.get("popularity");
+        }
+        catch (Exception err)
+        {
+            err.printStackTrace();
+        }
+    }*/
 }
