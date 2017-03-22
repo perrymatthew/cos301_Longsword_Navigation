3574d437c1909be794558b43f0c0cbcba6417205
Nathan Ngobale <youngjon01@gmail.com>
2017-03-23 01:00:03 +0200

    Update Routing.java

--- src_code/Routing.java
+++ src_code/Routing.java
@@ -1,4 +1,8 @@
 public class Routing implements Navigation
 {
     private DBRouteCache routeCache = new DBRouteCache();
+    
+    public void getFromGIS (ArrayList<Waypoint> gisList) {
+        listOfNodes = gisList;
+    }
 }
