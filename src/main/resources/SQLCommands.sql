7bf8faf684b6fb0366542f373800ab23be8f7574
Matthew Perry <matthew.perry.sa@gmail.com>
2017-04-13 15:24:47 +0200

    changed create routeCache table SQL command

--- src/main/resources/SQLCommands.sql
+++ src/main/resources/SQLCommands.sql
@@ -1,3 +1,10 @@
---Create table for RouteCache
-CREATE TABLE `RouteCache` ( `RouteID` INT NOT NULL AUTO_INCREMENT , `StartPoint` VARCHAR(20) NOT NULL , `EndPoint` VARCHAR(20) NOT NULL , `Popularity` INT NOT NULL , PRIMARY KEY (`RouteID`));
+CREATE TABLE `cos301`.`routecache` (
+  `idrouteCache` INT NOT NULL AUTO_INCREMENT COMMENT 'private key for table',
+  `routeString` VARCHAR(255) NOT NULL COMMENT 'the raw string of the route',
+  `startPoint` VARCHAR(45) NOT NULL COMMENT 'string for the route\'s starting node',
+  `endPoint` VARCHAR(45) NOT NULL COMMENT 'string for the route\'s ending node',
+  `popularity` INT NOT NULL COMMENT 'the popularity of the route',
+  PRIMARY KEY (`idrouteCache`),
+  UNIQUE INDEX `idrouteCache_UNIQUE` (`idrouteCache` ASC))
+COMMENT = 'This is the table used by Navigation to manage the caching of popular routes';
 
