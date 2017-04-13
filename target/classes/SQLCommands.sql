b09540c77ebfc6d1073c50f1a729e84a5e3f4a02
Marin Peroski <mperoski@outlook.com>
2017-04-13 17:21:11 +0200

    Add files via upload

---
+++ target/classes/SQLCommands.sql
@@ -0,0 +1,3 @@
+--Create table for RouteCache
+CREATE TABLE `RouteCache` ( `RouteID` INT NOT NULL AUTO_INCREMENT , `StartPoint` VARCHAR(20) NOT NULL , `EndPoint` VARCHAR(20) NOT NULL , `Popularity` INT NOT NULL , PRIMARY KEY (`RouteID`));
+
