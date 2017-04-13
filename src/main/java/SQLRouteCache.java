ea1167ac8b798222c35c0798193fb5998b9a04b6
Matthew Perry <matthew.perry.sa@gmail.com>
2017-04-13 16:11:20 +0200

    constructor implemented with DB connection

--- src/main/java/SQLRouteCache.java
+++ src/main/java/SQLRouteCache.java
@@ -1,5 +1,22 @@
 import java.sql.*;
 
 public class SQLRouteCache {
+    private final static String DB_URL = "jdbc:mysql://localhost/";
+    private final static String USERNAME = "admin";
+    private final static String PASSWORD = "root";
+    private final static String myDriver = "org.gjt.mm.mysql.Driver";
+    Connection connection;
+
+    public SQLRouteCache()
+    {
+        try {
+            connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            Class.forName(myDriver);
+            System.out.println("Connected to Database");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     
 }
