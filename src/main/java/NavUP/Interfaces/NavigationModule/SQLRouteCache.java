e0b1776450de28fd060d7eecabff81aacaadf634
Matthew Perry <matthew.perry.sa@gmail.com>
2017-04-13 18:10:06 +0200

    fixed comments to be inline with JavaDoc

--- src/main/java/NavUP/Interfaces/NavigationModule/SQLRouteCache.java
+++ src/main/java/NavUP/Interfaces/NavigationModule/SQLRouteCache.java
@@ -1,99 +1,103 @@
-//Package for Navigation Module
+/**
+ * @author Matthew Perry
+ */
+
+/**
+ * Package for Navigation Module
+ */
 package NavUP.Interfaces.NavigationModule;
 
-//Import libraries for SQL interaction
+/**
+ * Import libraries for SQL interaction
+ */
+
 import java.sql.*;
 
-//Class to manage SQL DB for most common routes
+/**
+ * Class to manage SQL DB for most common routes
+ */
 public class SQLRouteCache {
-    //Variables to connect to the DB
+    /**
+     * Variables to connect to the DB
+     */
     private final static String DB_URL = "jdbc:mysql://localhost/";
     private final static String USERNAME = "admin";
     private final static String PASSWORD = "root";
     private final static String myDriver = "org.gjt.mm.mysql.Driver";
     Connection connection;
 
-    //Default constructor
+    /**
+     * Default constructor
+     *
+     */
     public SQLRouteCache()
     {
-        //Try Catch block for error handling
         try {
-            //Establish connection to teh DB
             connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             Class.forName(myDriver);
             System.out.println("Connected to Database");
         }
         catch (Exception e){
-            //Throw exception if connection failed
             e.printStackTrace();
         }
     }
 
-    //Add route function to add the route to the SQL DB
-    public void addRoute(String route) throws SQLException {
-        //Variable for the start point
+    /**
+     * Add route function to add the route to the SQL DB
+     * @param route
+     */
+    public void addRoute(String route) {
         String start = "";
-        //Variable for the end point
         String end = "";
-        //Variable for the roues popularity
         int pop = 0;
 
-        //Try Catch block for error handling
         try {
-            //Adding the route to the DB
             String query = "insert into routecache (routeString, startPoint, endPoint, popularity)\n" + "values (\"" + route + "\",\"" + start + "\",\"" + end + "\"," + pop + ");";
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(query);
         }
         catch (SQLException e){
-            //Throw exception if connection failed
             e.printStackTrace();
         }
     }
 
-    //Remove route function to remove the route to the SQL DB
-    public void removeRoute(String route) throws SQLException {
-        //Variable for the start point
+    /**
+     * Remove route function to remove the route to the SQL DB
+     * @param route
+     */
+    public void removeRoute(String route) {
         String start = "";
-        //Variable for the end point
         String end = "";
-        //Variable for the roues popularity
         int pop = 0;
 
-        //Try Catch block for error handling
         try {
-            //Removing the route from the DB
             String query = "";
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(query);
         }
         catch (SQLException e){
-            //Throw exception if connection failed
             e.printStackTrace();
         }
     }
 
-    //Get route function to get the route to the SQL DB
-    public String getCachedRoute(String route) throws SQLException {
-        //Variable to store JSON route once found
-        String cahce = "";
-        //Variable for the start point
+    /**
+     * Get route function to get the route to the SQL DB
+     * @param route
+     * @return
+     */
+    public String getCachedRoute(String route) {
+        String cache = "";
         String start = "";
-        //Variable for the end point
         String end = "";
 
-        //Try Catch block for error handling
         try {
-            //Retrieve the route from the DB
             String query = "";
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(query);
         }
         catch (SQLException e){
-            //Throw exception if connection failed
             e.printStackTrace();
         }
-        //Return the JSON formatted route
-        return cahce;
+        return cache;
     }
 }
