bbb9feca341342a766aa91315a8f78fe53151908
Matthew Perry <matthew.perry.sa@gmail.com>
2017-04-14 22:01:41 +0200

    SQLRouteCache: +removeRoute() completed

--- src/main/java/NavUP/Interfaces/NavigationModule/SQLRouteCache.java
+++ src/main/java/NavUP/Interfaces/NavigationModule/SQLRouteCache.java
@@ -32,7 +32,7 @@ public class SQLRouteCache {
     private final static String USERNAME = "admin";
     private final static String PASSWORD = "root";
     private final static String myDriver = "org.gjt.mm.mysql.Driver";
-    private Connection connection;`
+    private Connection connection;
 
     /**
      * Default constructor
@@ -52,7 +52,7 @@ public class SQLRouteCache {
 
     /**
      * Add route function to add the route to the SQL DB
-     * @param route
+     * @param route this is a JSON String that serves as the route.
      */
     public void addRoute(String route) {
         String start;
@@ -76,31 +76,48 @@ public class SQLRouteCache {
                 insert.setString(2, start);
                 insert.setString(3, end);
                 insert.setInt(4, pop);
+                insert.executeUpdate();
             }
             catch (SQLException e){
-                System.out.println("Add route failure: " + e.toString());
+                System.out.println("Add route SQL failure: " + e.toString());
             }
         } catch (JSONException e) {
-            e.printStackTrace();
+            System.out.println("Add route JSON failure: " + e.toString());
         }
     }
 
     /**
-     * Remove route function to remove the route to the SQL DB
-     * @param route
+     * Remove route function to remove the route to the SQL DB.
+     * @param route this is a JSON String that serves as the route.
      */
     public void removeRoute(String route) {
-        String start = "";
-        String end = "";
-        int pop = 0;
+        String start;
+        String end;
 
         try {
-            String query = "";
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+            JSONObject json = new JSONObject(route);
+            JSONArray routeObj = json.getJSONArray("route");
+            int length = json.getInt("length");
+            start = routeObj.getJSONObject(0).getDouble("lat") + "," +
+                    routeObj.getJSONObject(0).getDouble("long");
+            end = routeObj.getJSONObject(length).getDouble("lat") + "," +
+                    routeObj.getJSONObject(length).getDouble("long");
+
+            try {
+                String query;
+                query = "DELETE FROM `routecache` WHERE startPoint=? AND endPoint=?;";
+                PreparedStatement remove = connection.prepareStatement(query);
+                remove.setString(1, start);
+                remove.setString(2, end);
+                remove.executeUpdate();
+
+            }
+            catch (SQLException e){
+                System.out.println("Remove route SQL failure: " + e.toString());
+            }
         }
-        catch (SQLException e){
-            e.printStackTrace();
+        catch (JSONException e) {
+            System.out.println("Remove route JSON failure: " + e.toString());
         }
     }
 
