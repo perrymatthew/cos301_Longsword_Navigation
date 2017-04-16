ba2711ad3ff80e0feaa76530aa5ea7b7a684b33f
Duart <duartbreedt@gmail.com>
2017-04-16 22:50:52 +0200

    Created SQLUserPins file and class

---
+++ src/main/java/NavUP/Interfaces/NavigationModule/SQLUserPins.java
@@ -0,0 +1,18 @@
+/**
+ * @author Duart Breedt
+ * @version 1
+ *
+ */
+
+package NavUP.Interfaces.NavigationModule;
+
+import java.sql.*;
+
+/**
+ * Class to manage user pins in the SQL database
+ */
+public class SQLUserPins {
+
+
+
+}
