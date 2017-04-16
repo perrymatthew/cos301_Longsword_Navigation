f7d5dc0854085f797215fcc98d86b7f7d7f80f04
Duart <duartbreedt@gmail.com>
2017-04-16 22:52:37 +0200

    SQLUserPins Constructor
    Created the constructor and connected to the SQL database

--- src/main/java/NavUP/Interfaces/NavigationModule/SQLUserPins.java
+++ src/main/java/NavUP/Interfaces/NavigationModule/SQLUserPins.java
@@ -13,6 +13,24 @@ import java.sql.*;
  */
 public class SQLUserPins {
 
+    /**
+     * Variables to connect to the DB
+     */
+    private final static String DB_URL = "jdbc:mysql://localhost/";
+    private final static String USERNAME = "admin";
+    private final static String PASSWORD = "root";
+    private final static String myDriver = "org.gjt.mm.mysql.Driver";
+    private Connection connection;
 
+    public SQLUserPins() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            Class.forName(myDriver);
+            System.out.println("Connected to Database");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
