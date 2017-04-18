e639f2495be09f488c5995cb72513897e005959f
Nathan Ngobale <youngjon01@gmail.com>
2017-04-18 15:26:28 +0200

    Update SQLUserPreferences.java

--- src/main/java/NavUP/Interfaces/NavigationModule/SQLUserPreferences.java
+++ src/main/java/NavUP/Interfaces/NavigationModule/SQLUserPreferences.java
@@ -25,3 +25,16 @@ import java.sql.*;
  * Class to manage SQL DB for user preferences and favourite routes
  *
  */
+public class SQLUserPreferences {
+    /**
+     * Variables to connect to the DB
+     */
+    private final static String DB_URL = "";
+    private final static String USERNAME = "admin";
+    private final static String PASSWORD = "root";
+    private final static String myDriver = "org.gjt.mm.mysql.Driver";
+    Connection connection;
+
+    /**
+     * Default constructor of SQLUserPreferences
+     */
