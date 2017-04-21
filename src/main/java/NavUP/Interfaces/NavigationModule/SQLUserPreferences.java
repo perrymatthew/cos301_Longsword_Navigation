52ebde44579aa2b556f6b0f6044610a980b0a5ac
Marin Peroski <mperoski@outlook.com>
2017-04-21 11:38:40 +0200

    Add files via upload

--- src/main/java/NavUP/Interfaces/NavigationModule/SQLUserPreferences.java
+++ src/main/java/NavUP/Interfaces/NavigationModule/SQLUserPreferences.java
@@ -11,7 +11,6 @@
 
 package NavUP.Interfaces.NavigationModule;
 
-import org.json.JSONObject;
 import java.sql.*;
 
 public class SQLUserPreferences {
@@ -41,10 +40,10 @@ public class SQLUserPreferences {
 
     /**
      * addUser function adds a user to the DB
-     * @param userId
-     * @param userRestriction
-     * @param userPreference
-     * @throws SQLException
+     * @param userId User id passed to function to identify the user
+     * @param userRestriction The user's restriction (disabled or not)
+     * @param userPreference The user's prefered route (with regards to the length)
+     * @throws SQLException Thrown exception in case the user can not be added 
      */
     public void addUser(String userId, boolean userRestriction, double userPreference) throws SQLException {
         try {
@@ -67,15 +66,9 @@ public class SQLUserPreferences {
      * @throws SQLException Thrown exception in case no update can be made
      */
     public void updatePreference(String userId, double userPreference) throws SQLException {
-        try
-        {
-          //use userId given from param
-            String query = "UPDATE preferences SET preferences=? WHERE userID =?";
+        try {
+            String query = "UPDATE preferences SET preferences=userID WHERE userID='" + userId + "'";
             PreparedStatement insert = connection.prepareStatement(query);
-
-            // set the preparedstatement parameters
-            insert.setDouble(1, userPreference);
-            insert.setString(2, userId);
             insert.executeUpdate();
         }
         catch (Exception e){
@@ -85,13 +78,13 @@ public class SQLUserPreferences {
 
     /**
      * updateRestrictions function to update the already existing user's restriction
-     * @param userID The user's ID to update
+     * @param userId The user's ID to update
      * @param userRestriction The restriction associated to the user's ID
      * @throws SQLException Thrown exception in case no update can be made
      */
-    public void updateRestrictions(String userID, boolean userRestriction) throws SQLException {
+    public void updateRestrictions(String userId, boolean userRestriction) throws SQLException {
         try {
-            String query = "UPDATE preferences SET restrictions = userRestriction WHERE userID = userID";
+            String query = "UPDATE preferences SET restrictions = userRestriction WHERE userID='" + userId + "'";
             PreparedStatement insert = connection.prepareStatement(query);
             insert.executeUpdate();
         }
@@ -104,12 +97,12 @@ public class SQLUserPreferences {
      * getUser function to get the user from the SQL DB.
      * @param userId The passed user ID to search from the DB
      * @return This is a string representing the user .
-     * @throws SQLException
+     * @throws SQLException Thrown exception in case no user can be obtained
      */
     public String getUser(String userId) throws SQLException {
         String user = "";
         try {
-            String query = "SELECT userID FROM `preferences` WHERE userID=userId";
+            String query = "SELECT userID FROM preferences WHERE userID='" + userId + "'";
             PreparedStatement select = connection.prepareStatement(query);
             ResultSet rs = select.executeQuery(query);
             user = rs.getString("userID");
@@ -129,10 +122,13 @@ public class SQLUserPreferences {
         String pref = "";
 
         try {
-            String query = "SELECT * FROM `preferences` WHERE userID=userId";
+            String query = "SELECT preferences FROM preferences WHERE userID='" + userId + "'";
             PreparedStatement select = connection.prepareStatement(query);
             ResultSet rs = select.executeQuery(query);
-            pref = rs.getString("preferences");
+
+            if(rs.next()){
+                pref = rs.getString(1);
+            }
         }
         catch (SQLException e){
             e.printStackTrace();
@@ -148,34 +144,17 @@ public class SQLUserPreferences {
     public String getRestrictions(String userId) {
         String restrictions = "";
         try {
-            String query = "SELECT * FROM preferences WHERE userID=userId";
+            String query = "SELECT restrictions FROM preferences WHERE userID='" + userId + "'";
             PreparedStatement select = connection.prepareStatement(query);
             ResultSet rs = select.executeQuery(query);
-            restrictions = rs.getString("restrictions");
+
+            if(rs.next()){
+                restrictions = rs.getString(1);
+            }
         }
         catch (SQLException e){
             e.printStackTrace();
         }
         return restrictions;
-    }
-    /**
-     * As stipulated from the preliminary umldiagram This method will remove  a User.
-     * @param userId This is the Unique ID for the user to be removed.
-     * @throws SQLException Thrown exception in case no deletion of user can be made
-     */
-    public void deleteUser(String userId){
-      try {
-        String sql ="DELETE FROM preferences WHERE userID = ?";
-        PreparedStatement pstmt = connection.prepareStatement(sql);
-        // set the corresponding param
-        pstmt.setInt(1, userId);
-          // execute the delete statement
-        pstmt.executeUpdate();
-
-      }
-      catch (Exception e){
-          e.printStackTrace();
-      }
-
     }
 }
